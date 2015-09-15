187263e8131b5bd9ee064f4a918474a043e617f0
Andriy <andkulak@gmail.com>
2015-09-14 23:33:17 -0400

    index.html, reddit.js and css with improved features

--- js/reddit.js
+++ js/reddit.js
@@ -1,31 +1,109 @@
 $(function(){
 
-	$(document).on("submit", '#cakedayForm', function(event) {
-		event.preventDefault();
-		var user = $("#userName").val();
-		displayDaysLeft(user);
-	});
-    
-    // fetching the date on which user name was created
-	var displayDaysLeft = function(user) {
-		user = $("#userName").val();
-		var userUrl = "http://www.reddit.com/user/" + user + "/about.json?jsonp=?";
+	"use strict";
 
-		$.getJSON(userUrl, function(data){
-			var createdDate = moment.unix(data.data.created);
-			var daysLeft = caluclateDaysLeft(createdDate);
-			$('#result').text(daysLeft);
-		});
+	var $result = $('#result');
+	var $message = $('#message');
+	var $form = $('#cakeday-form');
+	var $cakedayDate = $('#cakeday-date');
+	var $userNameInput = $('#user-name-input');
+
+	/**
+	 * requires person to enter user info
+	 */
+	var noInput = function(){
+		emptyAllElements();
+		$($message).html("You forgot to type a reddit username");
+		$($message).addClass('colored-background');
 	};
-	
-	//caluclating days left to the next reddit anniversary/birthday
-	var caluclateDaysLeft = function(createdDate) {
+
+	var gotInput = function(userName) {
+		var userUrl = createUserUrl(userName);
+		/**
+		 * If it takes more than a second to load, response will default to: not found
+		 */
+		$.ajax({
+			url: userUrl,
+			dataType: "jsonp",
+			timeout: 1000,
+			success: function(data){
+
+				var createdDate = moment.unix(data.data.created);
+				var nextCakeday = calculateNextCakeday(createdDate);
+
+				var daysLeft = nextCakeday.diff(moment(), 'days');
+
+				displayResults(nextCakeday, daysLeft);
+			},
+			error: function(){
+				emptyAllElements();
+				$($message).text("No. Could not find this one. Ask your parents");
+				$($message).after('<img src="css/img/sadpanda.png" id="panda" />')
+			}});
+	};
+	/**
+	 * Calculating days left until next b-day
+	 * @param createdDate
+	 * @returns {*}
+	 */
+	var calculateNextCakeday = function(createdDate){
 		var currentYear = moment().format('YYYY');
-		var nextBday = createdDate.year(currentYear);
+		var nextCakeday = createdDate.year(currentYear);
 
-		if(nextBday < moment()){
-			nextBday = nextBday.add('years', 1)
+		if(nextCakeday < moment()){
+			nextCakeday = nextCakeday.add('years', 1)
 		}
-		return nextBday.diff(moment(), 'days');
+		return nextCakeday;
+	}
+	/**
+	 * Display Result method
+	 * @param nextCakeday
+	 * @param daysLeft
+	 */
+	var displayResults = function(nextCakeday, daysLeft){
+		emptyAllElements();
+		displayNextCakeday(nextCakeday);
+		displayDaysLeft(daysLeft);
+		displayMessage(daysLeft);
+	}
+
+	var displayNextCakeday = function(nextCakeday) {
+		$($cakedayDate).text(nextCakeday.format("MMMM Do YYYY"));
 	};
+
+	var displayDaysLeft = function(daysLeft) {
+		$($result).text(daysLeft);
+	};
+
+	var displayMessage = function(daysLeft) {
+		var message = " ";
+
+
+
+		$($message).html("days left<br />" + message);
+	};
+
+	var emptyAllElements = function(){
+		$($cakedayDate).empty();
+		$($message).empty();
+		$($result).empty();
+		$('#panda').remove();
+		$($message).removeClass('colored-background');
+	};
+
+	var createUserUrl = function(userName){
+		return "http://www.reddit.com/user/" + userName +
+			"/about.json?jsonp=?";
+	};
+
+	$(document).on("submit", $form, function(event) {
+		event.preventDefault();
+		var userName = $userNameInput.val();
+
+		if(userName) {
+			gotInput(userName);
+		} else {
+			noInput();
+		}
+	});
 });
